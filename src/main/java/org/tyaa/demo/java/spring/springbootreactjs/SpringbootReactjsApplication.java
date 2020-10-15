94db1dd726438f269799cb256bbc03dbb6883852
YuriiTrofimenko <tyaa@ukr.net>
2020-10-15 16:37:29 +0300

    init, db config, first entities

---
+++ src/main/java/org/tyaa/demo/java/spring/springbootreactjs/SpringbootReactjsApplication.java
@@ -0,0 +1,13 @@
+package org.tyaa.demo.java.spring.springbootreactjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactjsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tyaa/demo/java/spring/springbootreactjs/SpringbootReactjsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tyaa.demo.java.spring.springbootreactjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
