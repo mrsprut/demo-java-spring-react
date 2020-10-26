9983ee487761fd47527a553e7aa319c2d9192126
YuriiTrofimenko <tyaa@ukr.net>
2020-10-26 16:06:57 +0200

    spring security: auth service and controller

--- src/main/java/org/tyaa/demo/java/spring/springbootreactjs/SpringbootReactjsApplication.java
+++ src/main/java/org/tyaa/demo/java/spring/springbootreactjs/SpringbootReactjsApplication.java
@@ -1,7 +1,16 @@
 package org.tyaa.demo.java.spring.springbootreactjs;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.tyaa.demo.java.spring.springbootreactjs.entities.Role;
+import org.tyaa.demo.java.spring.springbootreactjs.entities.User;
+import org.tyaa.demo.java.spring.springbootreactjs.repositories.RoleHibernateDAO;
+import org.tyaa.demo.java.spring.springbootreactjs.repositories.UserHibernateDAO;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class SpringbootReactjsApplication {
@@ -10,4 +19,45 @@ public class SpringbootReactjsApplication {
 		SpringApplication.run(SpringbootReactjsApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner initData(
+			RoleHibernateDAO roleDAO,
+			UserHibernateDAO userDAO,
+			PasswordEncoder passwordEncoder
+	) {
+		return args -> {
+			roleDAO.save(Role.builder().name("ROLE_ADMIN").build());
+			roleDAO.save(Role.builder().name("ROLE_USER").build());
+			Role adminRole = roleDAO.findRoleByName("ROLE_ADMIN");
+			Role userRole = roleDAO.findRoleByName("ROLE_USER");
+			userDAO.save(
+					User.builder()
+							.name("admin")
+							.password(passwordEncoder.encode("AdminPassword1"))
+							.role(adminRole)
+							.build()
+			);
+			userDAO.save(
+					User.builder()
+							.name("one")
+							.password(passwordEncoder.encode("UserPassword1"))
+							.role(userRole)
+							.build()
+			);
+			userDAO.save(
+					User.builder()
+							.name("two")
+							.password(passwordEncoder.encode("UserPassword2"))
+							.role(userRole)
+							.build()
+			);
+			userDAO.save(
+					User.builder()
+							.name("three")
+							.password(passwordEncoder.encode("UserPassword3"))
+							.role(userRole)
+							.build()
+			);
+		};
+	}
 }
--- src/main/java/org/tyaa/demo/java/spring/springbootreactjs/controllers/AuthController.java
+++ src/main/java/org/tyaa/demo/java/spring/springbootreactjs/controllers/AuthController.java
@@ -1,29 +1,119 @@
 package org.tyaa.demo.java.spring.springbootreactjs.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-import org.tyaa.demo.java.spring.springbootreactjs.entities.Role;
-import org.tyaa.demo.java.spring.springbootreactjs.entities.User;
+import org.tyaa.demo.java.spring.springbootreactjs.models.ResponseModel;
 import org.tyaa.demo.java.spring.springbootreactjs.models.RoleModel;
-import org.tyaa.demo.java.spring.springbootreactjs.repositories.RoleHibernateDAO;
-import org.tyaa.demo.java.spring.springbootreactjs.repositories.UserHibernateDAO;
-import org.tyaa.demo.java.spring.springbootreactjs.services.RoleService;
+import org.tyaa.demo.java.spring.springbootreactjs.models.UserModel;
+import org.tyaa.demo.java.spring.springbootreactjs.services.AuthService;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
-// @Controller
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
-    private RoleService roleService;
+    private AuthService authService;
 
-    @GetMapping("/roles")
-    // @ResponseBody
-    // @RequestMapping(path = "/index", method = RequestMethod.GET)
-    public List<RoleModel> getAllRoles() {
-        return roleService.getAllRoleModel();
+    /* @GetMapping("/roles")
+    public ResponseModel getAllRoles() {
+        return roleService.getAllRoles();
+    } */
+
+    /* @GetMapping("/role/users")
+    public ResponseModel getRoleUsers(@RequestParam("id") Long id) {
+        return roleService.getRoleUsers(id);
+    } */
+
+    /* @GetMapping("/roles/{id}/users")
+    public ResponseModel getRoleUsers(@PathVariable("id") Long roleId) {
+        return roleService.getRoleUsers(roleId);
+    } */
+
+    /* @GetMapping("/roles/{id}/users")
+    public ResponseModel getRoleUsers(@PathVariable Long id) {
+        return roleService.getRoleUsers(id);
+    } */
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/admin/roles")
+    public ResponseEntity<ResponseModel> getAllRoles() {
+        return new ResponseEntity<>(authService.getAllRoles(), HttpStatus.OK);
+    }
+
+    @Secured("ROLE_ADMIN")
+    @PostMapping("/admin/roles")
+    public ResponseEntity<ResponseModel> createRole(@RequestBody RoleModel roleModel) {
+        return new ResponseEntity<>(authService.createRole(roleModel), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/admin/roles/{id}")
+    public ResponseEntity<ResponseModel> deleteRole(@PathVariable Long id) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/admin/roles/{id}/users")
+    public ResponseEntity<ResponseModel> getUsersByRole(@PathVariable Long id) {
+        ResponseModel responseModel =
+                authService.getRoleUsers(id);
+        return new ResponseEntity<>(
+                responseModel,
+                (responseModel.getData() != null)
+                        ? HttpStatus.OK
+                        : HttpStatus.NOT_FOUND
+        );
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<ResponseModel> createUser(@RequestBody UserModel userModel) {
+        ResponseModel responseModel =
+                authService.createUser(userModel);
+        return new ResponseEntity<>(
+                responseModel,
+                (responseModel.getMessage().toLowerCase().contains("created"))
+                        ? HttpStatus.CREATED
+                        : responseModel.getMessage().contains("name")
+                        ? HttpStatus.CONFLICT
+                        : HttpStatus.BAD_GATEWAY
+        );
+    }
+
+    @DeleteMapping(value = "/user/{id}")
+    public ResponseEntity<ResponseModel> deleteUser(@PathVariable Long id) {
+        return new ResponseEntity<>(authService.deleteUser(id), HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping(value = "/user/check")
+    @ResponseBody
+    public ResponseEntity<ResponseModel> checkUser(Authentication authentication) {
+        ResponseModel responseModel = authService.check(authentication);
+        return new ResponseEntity<>(
+                responseModel,
+                (responseModel.getData() != null)
+                        ? HttpStatus.OK
+                        : HttpStatus.UNAUTHORIZED
+        );
+    }
+
+    @GetMapping("/user/signedout")
+    public ResponseEntity<ResponseModel> signedOut(HttpSession httpSession) {
+        return new ResponseEntity<>(authService.onSignOut(), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/onerror")
+    public ResponseEntity<ResponseModel> onError() {
+        return new ResponseEntity<>(authService.onError(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @Secured("ROLE_ADMIN")
+    @PatchMapping(value = "/users/{id}/makeadmin")
+    public ResponseEntity<ResponseModel> makeUserAdmin(@PathVariable Long id) throws Exception {
+        return new ResponseEntity<>(authService.makeUserAdmin(id), HttpStatus.OK);
     }
 }
